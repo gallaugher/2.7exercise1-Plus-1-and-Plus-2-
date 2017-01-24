33431ba0f6930968356c64d0421cc64a6f9c7212
Gallaugher <john.gallaugher@bc.edu>
2017-01-23 19:06:27 -0500

    Completed Chapter 2, Section 7, Exercise 1 - Plus 1 and Plus 2

--- Plus 1 and Plus 2/ViewController.swift
+++ Plus 1 and Plus 2/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    var index = 0
+// I could either set the text of the numberLabel within Interface Builder to equal "0", or I can set it in viewDidLoad. Try out both both.
+    // We didn't cover viewDidLoad, but this function, below, is written when we create a new view controller (essentially the app screen).  viewDidLoad executes when the app first loads into memory.  So this is a great place to do any initialization - setting up variables or other items for the start of your app.
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        numberLabel.text = String(index)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func plusOnePressed(_ sender: UIButton) {
+        index = index + 1
+        numberLabel.text = String(index)
+    }
 
+    @IBAction func plusTwoPressed(_ sender: UIButton) {
+        index = index + 2
+        numberLabel.text = String(index)
+    }
 }
 
